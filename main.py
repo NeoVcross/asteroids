57b5748147782ff380bc1a15e862cb6553b62ed4
Neocross <zhou-nicolas@hotmail.com>
2024-10-01 23:44:46 +0200

    add group

--- main.py
+++ main.py
@@ -8,16 +8,26 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+
+    Player.containers = (updatables, drawables)
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
-
+        for upd in updatables:
+            upd.update(dt)
+        
         screen.fill("black")
-        player.draw(screen)
+            
+        for draw in drawables:
+            draw.draw(screen)
+
         pygame.display.flip()
         
         # limit to 60 fps
